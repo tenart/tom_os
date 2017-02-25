1312313c1e2d618fda84e3de183875842a075771
Tom N <tomn98@dhcp-v880-5a-00443.1Xwireless.unc.edu>
2017-02-25 14:12:05 -0500

    Favicon Update

--- js/cipher.js
+++ js/cipher.js
@@ -6,6 +6,7 @@ $(function() {
     
     $("#encrypt").click(function() {
         var input = $("#input").val().toLowerCase();
+        var inputRaw = $("#input").val();
         var key = parseInt($("#key").val());
         var output = new Array(input.length);
         if( input.length > 0 && key != "" ) {
@@ -15,11 +16,14 @@ $(function() {
             for(i=0; i<input.length; i++) {
                 if(input[i] == " ") {
                     output[i] = input[i];
-                } else if( !isNaN(input[i]) ) {
+                } else if( alphabet.indexOf(input[i]) < 0 ) {
                     output[i] = input[i];
                 } else {
-// encrypted.append(alphabet[(alphabet.find(original[i].lower())+n)%26]);
-                    output[i] = alphabet[ (alphabet.indexOf(input[i]) + key)%26 ];
+                    var shiftedLetter = alphabet[ (alphabet.indexOf(input[i]) + key)%26 ];
+                    if( inputRaw[i] == input[i].toUpperCase() ) {
+                        var shiftedLetter = shiftedLetter.toUpperCase();
+                    }
+                    output[i] = shiftedLetter;
                 }
             }
             $("#output").val(output.join(""));
